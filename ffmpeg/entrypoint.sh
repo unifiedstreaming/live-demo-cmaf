1af895b7b2fac5dc9fbf701a3f6de6527a65d187
Dimitry Andric <dimitry@unified-streaming.com>
2019-07-19 13:23:14 +0200

    Attempt to better support low-latency DASH
    * Use shorter `dvr_window_length`
    * Add `--mpd.min_buffer_time` and `--mpd.suggested_presentation_delay`
      flags
    * Set GOPs to 24 frames, e.g. 0.960s at 25 fps
    * Update FFmpeg to version 4.1.4
    * Simplify entrypoint script
    * Obtain current time *once* and use that for both the `--ism_offset`
      flag and to adjust the timestamps that are burned into the video
    * Enable writing of ProducerReferenceTime (prft) boxes

--- ffmpeg/entrypoint.sh
+++ ffmpeg/entrypoint.sh
@@ -1,40 +1,68 @@
 #!/bin/sh
 
 # set env vars to defaults if not already set
-if [ ! $FRAME_RATE ]
-  then
-  export FRAME_RATE=25
-fi
+export FRAME_RATE="${FRAME_RATE:-25}"
+export GOP_LENGTH="${GOP_LENGTH:-${FRAME_RATE}}"
 
-if [ ! $GOP_LENGTH ]
-  then
-  export GOP_LENGTH=$FRAME_RATE
-fi
-
-export FRAME_SEP=\:
-if [ $FRAME_RATE == "30000/1001" ] || [ $FRAME_RATE == "60000/1001" ]
-  then
+if [ "${FRAME_RATE}" = "30000/1001" -o "${FRAME_RATE}" = "60000/1001" ]; then
   echo "drop frame"
-  export FRAME_SEP=\.
+  export FRAME_SEP="."
+else
+  export FRAME_SEP=":"
 fi
 
-if [ -z ${LOGO_OVERLAY+x$LOGO_OVERLAY} ]
-  then
-  export LOGO_OVERLAY="https://raw.githubusercontent.com/unifiedstreaming/live-demo/master/ffmpeg/usp_logo_white.png"
-fi
+export LOGO_OVERLAY="${LOGO_OVERLAY-https://raw.githubusercontent.com/unifiedstreaming/live-demo/master/ffmpeg/usp_logo_white.png}"
 
-if [ ! -z $LOGO_OVERLAY ]
-  then
+if [ -n "${LOGO_OVERLAY}" ]; then
   export LOGO_OVERLAY="-i ${LOGO_OVERLAY}"
   export OVERLAY_FILTER=", overlay=eval=init:x=W-15-w:y=15"
 fi
 
 # validate required variables are set
-if [ ! $PUB_POINT_URI ]
-  then
+if [ -z "${PUB_POINT_URI}" ]; then
   echo >&2 "Error: PUB_POINT_URI environment variable is required but not set."
   exit 1
 fi
 
+# get current time in microseconds
+DATE_MICRO=$(LANG=C date +%s.%6N)
+DATE_PART1=${DATE_MICRO%.*}
+DATE_PART2=${DATE_MICRO#*.}
+# the -ism_offset option has a timescale of 10,000,000, so add an extra zero
+ISM_OFFSET=${DATE_PART1}${DATE_PART2}0
+# the number of seconds into the current day
+DATE_MOD_DAYS=$((${DATE_PART1}%86400))
 
-exec "$@"
+set -x
+exec ffmpeg \
+  -re \
+  -nostats \
+  -f lavfi -i smptehdbars=size=1280x720 \
+  -f lavfi -i anullsrc \
+  ${LOGO_OVERLAY} \
+  -filter_complex "\
+    drawtext=\
+      box=1:\
+      boxborderw=4:\
+      boxcolor=black:\
+      fontcolor=white:\
+      fontsize=32:\
+      text='%{pts\:gmtime\:${DATE_PART1}\:%Y-%m-%d}%{pts\:hms\:${DATE_MOD_DAYS}.${DATE_PART2}}':\
+      x=(w-tw)/2:\
+      y=30\
+      " \
+  -g ${GOP_LENGTH} \
+  -r ${FRAME_RATE} \
+  -keyint_min ${GOP_LENGTH} \
+  -c:v libx264 \
+  -preset veryfast \
+  -profile:v baseline \
+  -c:a aac \
+  -map 0:v \
+  -map 1:a \
+  -fflags +genpts \
+  -movflags isml+frag_keyframe \
+  -write_prft pts \
+  -ism_offset ${ISM_OFFSET} \
+  -f ismv \
+  ${PUB_POINT_URI}
