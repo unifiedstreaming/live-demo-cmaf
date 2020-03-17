af1f0a3f1649531785ce3dc9a5e2f3e35cac1bc5
rufael <roefi20@gmail.com>
2020-03-17 17:04:17 +0100

    ffmpeg for cmaf ingest

--- ffmpeg/entrypoint.sh
+++ ffmpeg/entrypoint.sh
@@ -34,35 +34,24 @@ ISM_OFFSET=${DATE_PART1}${DATE_PART2}0
 DATE_MOD_DAYS=$((${DATE_PART1}%86400))
 
 set -x
-exec ffmpeg \
-  -re \
-  -nostats \
-  -f lavfi -i smptehdbars=size=1280x720 \
-  -f lavfi -i anullsrc \
-  ${LOGO_OVERLAY} \
-  -filter_complex "\
-    drawtext=\
-      box=1:\
-      boxborderw=4:\
-      boxcolor=black:\
-      fontcolor=white:\
-      fontsize=32:\
-      text='%{pts\:gmtime\:${DATE_PART1}\:%Y-%m-%d}%{pts\:hms\:${DATE_MOD_DAYS}.${DATE_PART2}}':\
-      x=(w-tw)/2:\
-      y=30\
-      " \
-  -g ${GOP_LENGTH} \
-  -r ${FRAME_RATE} \
-  -keyint_min ${GOP_LENGTH} \
-  -c:v libx264 \
-  -preset veryfast \
-  -profile:v baseline \
-  -c:a aac \
-  -map 0:v \
-  -map 1:a \
-  -fflags +genpts \
-  -movflags isml+frag_keyframe \
-  -write_prft pts \
-  -ism_offset ${ISM_OFFSET} \
-  -f ismv \
-  ${PUB_POINT_URI}
+exec ffmpeg -re -f lavfi -i smptehdbars=size=1280x960:rate=25 -i  usp_logo_white.png -filter_complex "\
+	[0:v]drawbox=y=25:\
+       	x=iw/2-iw/7:\
+       	c=0x00000000@1:\
+       	w=iw/3.5: h=36:\
+       	t=max,drawtext=timecode_rate=${FRAME_RATE}:\
+       	timecode='$(date -u +%H\\:%M\\:%S)\\${FRAME_SEP}$(($(date +%3N)/$(($FRAME_RATE))))':\
+       	tc24hmax=1: fontsize=32: x=(w-tw)/2+tw/2: y=30:\
+       	fontcolor=white, drawtext=text='%{gmtime\:%Y-%m-%d}\ ':\
+       	fontsize=32: x=(w-tw)/2-tw/2: y=30:\
+       	fontcolor=white${OVERLAY_FILTER},split=3[out1][out2][out3]" \
+    -map "[out1]" -s 1280x960 -b:v 1000k -an -g 24 -r 25 -keyint_min 24 \
+	-c:v libx264 -profile main -preset ultrafast -tune zerolatency -fflags +genpts \
+    -movflags frag_keyframe+empty_moov+separate_moof+default_base_moof \
+	-f mp4 "$PUB_POINT/Streams(video2-1280-1000k.cmfv)" \
+    -map "[out2]" -s 640x480 -b:v 600k -an  -g 24 -r 25 -keyint_min 24 -preset ultrafast -tune zerolatency \
+	-c:v libx264  -fflags +genpts -movflags frag_keyframe+empty_moov+separate_moof+default_base_moof \
+	-f mp4  "$PUB_POINT/Streams(video2-640-600k.cmfv)" \
+    -map "[out3]" -s 320x240 -b:v 400k -an  -g 24 -r 25 -keyint_min 24 -preset ultrafast -tune zerolatency \
+	-c:v libx264 -fflags +genpts -movflags frag_keyframe+empty_moov+separate_moof+default_base_moof \
+	-f mp4 "$PUB_POINT/Streams(video2-320-400k.cmfv)"
