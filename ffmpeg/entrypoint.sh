7b91d33e3cc4834b8b51f0be1a4b7aa02334d347
Rufael Mekuria <roefi20@gmail.com>
2020-03-18 09:05:39 +0100

    Update entrypoint.sh

--- ffmpeg/entrypoint.sh
+++ ffmpeg/entrypoint.sh
@@ -42,8 +42,8 @@ exec ffmpeg -re -f lavfi -i smptehdbars=size=1280x960:rate=$FRAME_RATE -re \
        	tc24hmax=1: fontsize=32: x=(w-tw)/2+tw/2: y=30:\
        	fontcolor=white, drawtext=text='%{gmtime\:%Y-%m-%d}\ ':\
        	fontsize=32: x=(w-tw)/2-tw/2: y=30:\
-       	fontcolor=white${OVERLAY_FILTER}" \
-       	-map 0:v -s 1280x960 -b:v 1200k -an -g $GOP_SIZE -r $FRAME_RATE -keyint_min $GOP_SIZE \
+       	fontcolor=white,overlay=eval=init:x=W-15-w:y=15" \
+       	-map 0:v -s 1280x960 -b:v 1200k -an -g $GOP_LENGTH -r $FRAME_RATE -keyint_min $GOP_SIZE \
 	-c:v libx264 -profile:v main -preset ultrafast -tune zerolatency  -fflags +genpts \
         -movflags +frag_keyframe+empty_moov+separate_moof+default_base_moof \
 	-f mp4 "$PUB_POINT/Streams(video2-1280-1000k.cmfv)" \
