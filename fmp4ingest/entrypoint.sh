d4816d113563a1f64132ae6695ce5f0dae87dfc4
boy-vd <boy@unified-streaming.com>
2020-09-25 12:55:04 +0200

    too much sleep isn't good for you...

--- fmp4ingest/entrypoint.sh
+++ fmp4ingest/entrypoint.sh
@@ -6,6 +6,7 @@ seconds_since_epoch=$(date +%s)
 integer_multiple_gops_since_epoch=$(expr $seconds_since_epoch / $INTEGER_MULTIPLE_GOP_LENGTH)
 ism_offset=$(expr $(expr $integer_multiple_gops_since_epoch + 1) \* $INTEGER_MULTIPLE_GOP_LENGTH)
 
-sleep $(expr $INTEGER_MULTIPLE_GOP_LENGTH - 1)
+sleep_seconds=$(( $ism_offset - $seconds_since_epoch - 1 ))
+sleep $sleep_seconds
 
-/home/fmp4-ingest/ingest-tools/fmp4ingest -r --ism_offset ${ism_offset} -u $PUB_POINT_URI --avail $AVAIL_INTERVAL $AVAIL_LENGTH $CMFT
+/home/fmp4-ingest/ingest-tools/fmp4ingest -r --ism_offset $ism_offset -u $PUB_POINT_URI --avail $AVAIL_INTERVAL $AVAIL_LENGTH $CMFT
