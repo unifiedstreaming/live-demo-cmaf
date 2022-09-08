cd8b61b6c70cad0a95277f8716f626d864191813
Rufael Mekuria <roefi20@gmail.com>
2022-09-08 10:45:51 +0200

    Update entrypoint.sh
    remove the custom logic for the epoch synch

--- fmp4ingest/entrypoint.sh
+++ fmp4ingest/entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -x
 
-seconds_since_epoch=$(date +%s)
-integer_multiple_gops_since_epoch=$(expr $seconds_since_epoch / $INTEGER_MULTIPLE_GOP_LENGTH)
-ism_offset=$(expr $(expr $integer_multiple_gops_since_epoch + 1) \* $INTEGER_MULTIPLE_GOP_LENGTH)
-
-sleep_seconds=$(( $ism_offset - $seconds_since_epoch - 1 ))
-sleep $sleep_seconds
+sleep 5
 
 /home/fmp4-ingest/ingest-tools/push_markers --track_id 100 --announce 4000 --seg_dur 1920 --vtt -r -u $PUB_POINT_URI --avail $AVAIL_INTERVAL $AVAIL_LENGTH 
